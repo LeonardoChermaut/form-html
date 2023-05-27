4192b03e4759f1d64d2d8e3078b886201ef947a4
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-05-27 20:31:26 -0300

    start

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# form-register-html
+# Formulário de Registro
+
+Este é um simples formulário de registro para uma vaga de emprego. O formulário permite que os usuários preencham informações pessoais, como nome, idade, cargo pretendido, e-mail e número de telefone. O objetivo é capturar os dados do usuário e exibi-los em um console.
+
+## Tecnologias utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Estrutura do projeto
+
+O projeto consiste em um arquivo HTML, um arquivo CSS e um arquivo JavaScript. Aqui está uma visão geral da estrutura do projeto:
+
+- `index.html`: Este arquivo contém a estrutura HTML do formulário de registro, incluindo os campos de entrada e o botão de envio. Também são definidas algumas meta tags para otimização de mecanismos de busca e visualização em dispositivos móveis.
+
+- `style.css`: Este arquivo contém os estilos CSS para estilizar o formulário de registro. Ele segue as melhores práticas de CSS moderno, como reset de estilos, estilos responsivos e boas práticas de nomenclatura.
+
+- `index.js`: Este arquivo contém o código JavaScript responsável pela validação do formulário, formatação dos campos de entrada e manipulação dos dados inseridos. Ele também exibe uma mensagem de confirmação após o envio do formulário e limpa os campos do formulário.
+
+## Como executar o projeto
+
+1. Faça o download ou clone este repositório em seu computador.
+
+2. Abra o arquivo `index.html` em um navegador web de sua preferência.
+
+3. Preencha os campos do formulário com as informações solicitadas.
+
+4. Clique no botão "Enviar" para enviar o formulário.
+
+5. Os dados preenchidos no formulário serão exibidos no console do navegador.
+
+## Considerações finais
+
+Este é um projeto simples para demonstrar o uso básico de HTML, CSS e JavaScript em um formulário de registro. Sinta-se à vontade para explorar e personalizar o código de acordo com suas necessidades.
+
+**Observação:** Este projeto não possui backend, portanto, os dados não são salvos ou enviados a nenhum servidor. A funcionalidade é apenas para exibição dos dados no console do navegador.
---
+++ src/js/index.js
@@ -0,0 +1,102 @@
+const formInputs = {
+    form: document.getElementById("form-user"),
+    name: document.getElementById("name"),
+    email: document.getElementById("email"),
+    age: document.getElementById("age"),
+    job: document.getElementById("job"),
+    phone: document.getElementById("phone"),
+    createdAt: (document.getElementById("created-at").value = new Date().toLocaleDateString()),
+  };
+  
+  const TEXT_PATHERN = /[^A-Za-zÀ-ÿ\s ]/g;
+  const PHONE_REGEX = /(\d{0,2})(\d{0,1})(\d{0,4})(\d{0,4})/;
+  const { form, name, phone, age, job } = formInputs;
+  const formFields = ["name", "email", "phone"];
+  let formUser = {};
+  
+  const showMessageRequest = (message) => alert(message);
+  
+  const attributes = {
+    minLength: (input) => input.getAttribute("minlength"),
+    maxLength: (input) => input.getAttribute("maxlength"),
+  };
+  
+  const showDataRegister = () => {
+    const register = handleValuesFormField();
+    console.table(register);
+  };
+  
+  const setCustomValidity = (input, message) => {
+    return input.setCustomValidity(message || "");
+  };
+  
+  const validateInputLength = (input, minLength, maxLength) => {
+    const { value: { length: characters } } = input;
+    if (characters < minLength) {
+      setCustomValidity(input, `O de registro deve ter no mínimo ${minLength} caracteres.`);
+    }
+    else if (characters > maxLength) {
+      setCustomValidity(input, `O de registro deve ter no máximo ${maxLength} caracteres.`);
+    }
+    else {
+      setCustomValidity(input);
+    }
+  };
+  
+  const formatPhoneNumber = (number) => {
+    const cleanedPhoneNumber = number.replace(/\D/g, "");
+    const match = cleanedPhoneNumber.match(PHONE_REGEX);
+    if (!match[2]) return match[1];
+  
+    return `(${match[1]})${match[2]} ${match[3]}-${match[4]}`;
+  };
+  
+  const handleValuesFormField = () => {
+    formFields.forEach((field) => (formUser[field] = formInputs[field].value));
+    formUser["createdAt"] = formInputs["createdAt"];
+  
+    return formUser;
+  };
+  
+  const clearForm = () => formFields.forEach(element => formInputs[element].value = "");
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    showDataRegister();
+    showMessageRequest("Obrigado por seu cadastro!");
+    clearForm();
+  };
+  
+  const initializeApp = () => {
+
+    const addInputEventListeners = (...inputs) => {
+      inputs.forEach((input) => {
+        input.addEventListener("input", function (e) {
+          const letters = e.target.value.replace(TEXT_PATHERN, "");
+          validateInputLength(this, attributes.minLength(this), attributes.maxLength(this));
+          e.target.value = letters;
+        });
+      });
+    };
+    addInputEventListeners(name, job);
+    
+    
+    phone.addEventListener("input", function (e) {
+      const { value: number } = e.target;
+      validateInputLength(this, attributes.minLength(this), attributes.maxLength(this));
+  
+      return (e.target.value = formatPhoneNumber(number));
+    });
+    
+    form.addEventListener("submit", (e) => handleSubmit(e));
+  };
+  
+  const handleDOMContentLoadedApp = () => {
+    try {
+      initializeApp();
+    } catch (error) {
+      console.error("[error]: ", error);
+    }
+  };
+  window.addEventListener("DOMContentLoaded", handleDOMContentLoadedApp);
+  
